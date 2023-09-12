a348f5c83070cf9e963d7be4e1aa71aea9481305
Pasha <100295341+iaaka@users.noreply.github.com>
2023-09-12 10:07:36 +0100

    Update 01.run.estimate.signatures.sh
    update c2l to v0.1.3

--- src/01.run.estimate.signatures.sh
+++ src/01.run.estimate.signatures.sh
@@ -25,7 +25,7 @@
 
 
 WDIR=`pwd -P`
-IMAGE=/nfs/cellgeni/singularity/images/c2l.jhub.221206.v0.1.sif # based on commit 36e4f007e8fba4cb85c13b9bff47a4f6fbae9295
+IMAGE=/nfs/cellgeni/singularity/images/c2l_v0.1.3.sif #or use c2l.jhub.221206.v0.1.sif which is based on commit 36e4f007e8fba4cb85c13b9bff47a4f6fbae9295
 c2lref=./actions/c2l/src/py/01.estimate.signatures.py
 
 # edit below
