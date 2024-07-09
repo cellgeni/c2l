4b472f68f69a404149c0f14adc679eddc33c96cd
Pavel Mazin <iaa.aka@yandex.ru>
2024-07-09 21:23:47 +0100

    add iget_spaceranger.sh

--- src/iget_spaceranger.sh
+++ src/iget_spaceranger.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -e
 
-while read -r line; do
-	name=`basename $line`
+while read -r name line; do
+	#name=`basename $line`
 	echo "download $name"
 	mkdir $name
 	cd $name
-	iget -r $line/raw_feature_bc_matrix
 	iget -r $line/web_summary.html
 	iget -r $line/spatial
 	iget -r $line/filtered_feature_bc_matrix
 	iget -r $line/filtered_feature_bc_matrix.h5
+	iget -r $line/raw_feature_bc_matrix
 	iget -r $line/raw_feature_bc_matrix.h5
+	iget -r $line/cloupe.cloupe
+	iget -r $line/_cmdline
 	iget -r $line/metrics_summary.csv
 	cd ..
 done
