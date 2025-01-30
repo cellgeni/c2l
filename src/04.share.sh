df408c2e24f869a101bc9797b627b793a16f6bb0
Pavel Mazin <iaa.aka@yandex.ru>
2025-01-30 11:57:41 +0000

    add total coverage calculation into preparation; update shring script

--- src/04.share.sh
+++ src/04.share.sh
@@ -4,22 +4,18 @@
 tic=`pwd | grep -o '[^/]*$'`
 name=`cut -f2 actions/SUBMITTER`
 
-rsync -auR --progress pred /warehouse/cellgeni/$tic/
-rsync -auR --progress ref /warehouse/cellgeni/$tic/
-rsync -auR --progress figures /warehouse/cellgeni/$tic/
-
-echo "data were copied to /warehouse/cellgeni/$tic"
-echo "==========================="
-
 echo "Hi [~${name}],"
-echo "The analysis is finished, you can find results here: /warehouse/cellgeni/$tic, predictions are in pred subfolder. Several variants of prediction are available, they are combinations of following factors:"
+echo "The analysis is finished, you can find results here: /lustre/scratch127/cellgen/cellgeni/tickets/$tic, predictions are in pred subfolder. Several variants of prediction are available, they are combinations of following factors:"
 echo "1) detection alpha 20 or 200"
 echo "2) all celltypes or only celltypes with number of cells above 20 (filtered)"
-echo "3) with or without percent_mito covariate (mt)"
+echo "3) different annotation"
+echo "4) different covariates"
 echo 
 echo "Vitalii suggest to use alpha=20 (but previous recommendation was 200)."
 echo
-echo "Outputs of c2l are stored as csv files, recommended estimates of cell abundances are in predmodel/q05_cell_abundance_w_sf.csv."
+echo "Outputs of c2l are stored as csv files, recommended estimates of cell abundances are in predmodel/q05_cell_abundance_w_sf.csv.  Alternatively you can use predmodel/sp.h5ad output."
 echo "There are some summary figures in figures subfloder and QC figures in prediction subfolders."
+echo
+echo "Reference signatures can in found in red fubfolder"
 echo
 echo "Please let me know if you have any questions or requests."
